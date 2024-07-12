b741e1382dee3991619cce9dd1aee36c47d55b73
ROQYYB <125754105+roqyyb@users.noreply.github.com>
2024-07-12 07:29:44 +0100

    Implement the getComputerChoice logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+// get random choice from : rock || paper || scissors
+function getComputerChoice() {
+  // get random number
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  // setting choice based on "randomNumber"
+  const choice =
+    randomNumber === 1 ? "rock" : randomNumber === 2 ? "paper" : "scissors";
+  return choice;
+}
+
+console.log(getComputerChoice());
